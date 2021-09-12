205cee7470847cd26041cc2a2923ee6b598ba3d7
themagicteeth <will.js@protonmail.com>
2021-09-12 18:04:14 -0500

    Add defense magic shop

---
+++ userscripts/autobuy/defenceMagic.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name        Defense Magic
+// @namespace   Neopetscripts
+// @match       *://www.neopets.com/objects.phtml?obj_type=10&type=shop
+// @match       *://www.neopets.com/objects.phtml?type=shop&obj_type=10
+// @match       *://www.neopets.com/haggle.phtml
+// @match       *://www.neopets.com/haggle.phtml*
+// @grant       none
+// @version     1.0
+// @author      -
+// @require     https://raw.githubusercontent.com/themagicteeth/newneo/main/userscripts/autobuy/autoBuy.js
+// @description Auto buy for Defense
+// ==/UserScript==
+
+
+const itemsToBuy = [
+    "Thyoras Tear",
+    "Rainbow Scorchstone",
+    "Purple Scorchstone",
+    "Squishy Shoyru Healing Stone",
+    "Bag of Lenny Healing Seeds",
+    "Kaylas Magic Cloak",
+    "Superior Reflection Shield",
+    "Ultra Dual Shovel",
+    "Ultimate Dark Reflectorb",
+    "Flame Reflectozap",
+    "Mega U-Bend",
+    "Triple Turbo Dryer"
+  ]
+  
+  if (document.URL.includes("objects")) {
+   const FROM = 9000
+   const TO = 17000
+  
+   const refreshTimes = Math.floor(Math.random() * parseFloat(TO - FROM)) + parseFloat(FROM)
+  
+   buyItem(itemsToBuy, refreshTimes) 
+  }
